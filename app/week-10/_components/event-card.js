caeba8002ac1e3d0f5965f870e7499e61361e7da
Aaron Warsylewicz <aaron@warsylewicz.ca>
2023-11-30 17:19:30 -0700

    Update event-card.js

--- app/week-10/_components/event-card.js
+++ app/week-10/_components/event-card.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function EventCard({ event }) {
   return (
-    <Link href={`/week10/${event.id}`}>
+    <Link href={`/week-10/${event.id}`}>
       <div className="cursor-pointer border border-blue-500 bg-blue-800 m-1 p-2">
         <h3 className="text-xl font-bold">{event.name}</h3>
         <p>Date: {event.date.toDateString()}</p>
